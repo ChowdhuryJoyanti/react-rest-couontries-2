e0836b234cf8c3baf0355e408b8cc7974c58131a
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-03-24 21:29:41 +0600

    last added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
 
@@ -13,17 +14,20 @@ const Countries = () => {
     },[])
 
     return (
-        <div>
+        <div >
             <h1>Hello from Countries :{countries.length }</h1>
-            {
+            {/* {
                 countries.map(country => console.log(country))
-            }
-            {
+            } */}
+           <div className='countries-container'>
+           {
                 countries.map(country => <Country 
                     country ={country}
-                    
+                    key={country.cca3}
+
                     ></Country>)
             }
+           </div>
 
             {/* {
                 countries.map(country => <Country 
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area,region,name} = props.country;
+    const {area,region,name,flags} = props.country;
+    console.log(props.country);
     // console.log(props.country);
     // const {area,region,population,name} = props.country;
     return (
@@ -13,6 +14,7 @@ const Country = (props) => {
             <p><small>Region:{region}</small></p> */}
 
             <h2>Name:{name.common}</h2>
+            <img src={flags.png} alt="" />
             <p>Area:{area}</p>
             <p>Region:{region}</p>
         </div>
